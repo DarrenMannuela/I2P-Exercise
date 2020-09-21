b018ed259bc30fdde2c6da65c778154bca1505d8
DarrenMannuela <70499659+DarrenMannuela@users.noreply.github.com>
2020-09-21 12:37:37 +0700

    Add files via upload

---
+++ I2P Exercise Part 2 no.2.py
@@ -0,0 +1,13 @@
+student_amount=3
+
+student1=int(input("Pleas input Student1's score:"))
+student2=int(input("Pleas input Student2's score:"))
+student3=int(input("Pleas input Student3's score:"))
+average=(student1+student2+student3)/student_amount
+
+
+print("Student Scores:")
+print(student1)
+print(student2)
+print(student3)
+print("Average Score of Class:",average)
---
+++ I2P Exercise Part 2 no.3.py
@@ -0,0 +1,18 @@
+class_1=32
+class_2=45
+class_3=51
+
+class1_groups=5
+class2_groups=7
+class3_groups=6
+
+
+print("Number of students in each group: ")
+print("Class 1:",class_1//class1_groups)
+print("Class 2:",class_2//class2_groups)
+print("Class 3:",class_3//class3_groups)
+
+print("Number of students left over: ")
+print("Class 1:",class_1%class1_groups)
+print("Class 2:",class_2%class2_groups)
+print("Class 3:",class_3%class3_groups)
