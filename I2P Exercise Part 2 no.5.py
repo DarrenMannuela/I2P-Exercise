937b34e20cbe5e1a726364882807b30906bb6ad6
DarrenMannuela <70499659+DarrenMannuela@users.noreply.github.com>
2020-09-21 12:59:07 +0700

    Delete I2P Exercise Part 2 no.5.py

--- I2P Exercise Part 2 no.5.py
+++
@@ -1,7 +0,0 @@
-frequency=int(input("Please input the value of frequency(Hz):"))
-velocity=int(input("Please input the value of velocity(m/s):"))
-wavelength=velocity/frequency
-
-print("The speed(m/s):",velocity)
-print("The frequency(Hz):",frequency)
-print("The wavelength(m):",wavelength)
